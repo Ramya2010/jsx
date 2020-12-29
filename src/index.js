74bcea60967a97c23d5fbe3507d5a20dac787419
Ramya2010 <47886253+Ramya2010@users.noreply.github.com>
2020-12-29 23:36:59 +0530

    date and copywrite

--- src/index.js
+++ src/index.js
@@ -6,3 +6,17 @@
 //E.g.
 //Created by Angela Yu.
 //Copyright 2019.
+import React from "react";
+import ReactDOM from "react-dom";
+const name = "Ramya";
+const currentdate = new Date();
+console.log(currentdate);
+const year = currentdate.getFullYear();
+
+ReactDOM.render(
+  <div>
+    <p>Created by {name}</p>
+    <p>Copywrite {year}</p>
+  </div>,
+  document.getElementById("root")
+);
